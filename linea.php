3025ab9434551cfc5b5f2b43f150799b5b67db3f
SMM1799 <sm.maldonado1799@gmail.com>
2024-09-09 20:51:03 -0600

    Se redujo el tamaño de la letra en la pagina de la linea 3 para las pantallas medianas, grandes y extragrandes

--- linea.php
+++ linea.php
@@ -71,7 +71,7 @@ get_header();
 
 
 <section class="relative w-full min-h-screen flex flex-col items-center justify-center text-black bg-[#E9E9E9] px-7 sm:px-10 xl:px-32 py-20 gap-5">
-<h2 class="text-[#1D3750] text-[1.575rem] text-center sm:text-3xl xl:text-4xl 2xl:text-6xl font-extrabold lg:font-bold tracking-wide mb-4">
+<h2 class="text-[#1D3750] text-[1.575rem] text-center sm:text-3xl xl:text-3xl 2xl:text-4xl min-[1920px]:text-5xl font-extrabold lg:font-bold tracking-wide mb-4">
 PROYECTOS REALIZADOS
 </h2>
 
@@ -102,21 +102,21 @@ PROYECTOS REALIZADOS
      class="sm:w-1/3"/>
 </ul>
 
-<h3 class="text-3xl sm:text-2xl xl:text-3xl 2xl:text-5xl font-bold tracking-wide">
+<h3 class="text-3xl sm:text-2xl xl:text-2xl 2xl:text-3xl font-bold tracking-wide">
 PROCESO
 </h3>
 
-<p class="text-lg sm:text-sm md:text-base xl:text-xl 2xl:text-3xl">
+<p class="text-lg sm:text-sm md:text-base xl:text-lg 2xl:text-xl min-[1920px]:text-2xl">
 El financiamiento a través de la Línea 3, se lleva a cabo por medio de la evaluación y monitoreo de una entidad administradora, en este caso CUFRISA, la cual se encarga de evaluar la viabilidad técnica y financiera de los proyectos y, de ser factibles, la administración los recursos para paquetes de viviendas.
 
 
 </p>
 
-<h4 class="text-lg  xl:text-xl 2xl:text-3xl font-bold ">
+<h4 class="text-lg  xl:text-lg 2xl:text-xl min-[1920px]:text-2xl font-bold ">
 PASOS PARA INICIAR TU PROYECTO
 </h4>
 
-<p class="text-lg sm:text-sm md:text-base xl:text-xl 2xl:text-3xl">
+<p class="text-lg sm:text-sm md:text-base xl:text-lg 2xl:text-xl min-[1920px]:text-2xl">
 El Consejo de Administración del INFONAVIT, autoriza el paquete de Línea 3 dede las viviendas a desarrollar y autoriza de manera preliminar a la empresa constructora.
 <br/>
 <br/>
@@ -137,17 +137,17 @@ La empresa Constructora entrega a CUFRISA la información Técnica, Jurídica y 
 <br/>
 
 
-<h3 class="text-xl sm:max-md:text-lg xl:text-2xl 2xl:text-4xl text-center font-bold tracking-wide">
+<h3 class="text-xl sm:max-md:text-lg xl:text-xl 2xl:text-2xl min-[1920px]:text-3xl text-center font-bold tracking-wide">
 PRESENTACIÓN A DETALLE DE CRÉDITO INTEGRAL     
 </h3>
 
 <article class="flex flex-col sm:flex-row bg-[#DEDEDE] p-5 2xl:p-6 w-full items-center mb-6 rounded gap-7 sm:gap-0 sm:justify-between">
-<p class="text-center sm:text-left text-xl sm:text-sm md:text-lg lg:text-xl 2xl:text-3xl sm:w-7/12 lg:w-9/12">
+<p class="text-center sm:text-left text-xl sm:text-sm md:text-lg lg:text-lg xl:text-xl 2xl:text-2xl min-[1920px]:text-3xl sm:w-7/12 lg:w-9/12">
 Crédito Integral CUFRISA (pdf)
  </p>
  <a href="<?php echo get_template_directory_uri(); ?>/assets/pdf/Promocion Credito Integral CUFRISA 4.pdf" 
     target="_blank" 
-    class="flex sm:flex-row justify-center bg-[#263650] w-11/12 sm:w-4/12 xl:w-3/12 text-center text-white text-xl sm:text-sm md:text-lg lg:text-xl 2xl:text-3xl py-4 2xl:py-6 rounded hover:bg-blue-600 transition-colors duration-300">
+    class="flex sm:flex-row justify-center bg-[#263650] w-11/12 sm:w-4/12 xl:w-3/12 text-center text-white text-xl sm:text-sm md:text-lg lg:text-xl xl:text-xl 2xl:text-2xl min-[1920px]:text-3xl py-4 2xl:py-6 rounded hover:bg-blue-600 transition-colors duration-300">
   Descargar
   <img src="<?php echo get_template_directory_uri(); ?>/public/descargas.svg" 
        loading="lazy" 
@@ -157,18 +157,20 @@ Crédito Integral CUFRISA (pdf)
 </article>
 
 
-<h3 class="text-center text-2xl sm:max-md:text-lg 2xl:text-4xl font-bold tracking-wide mb-4 sm:mb-0 ">
+<h3 class="text-center text-2xl sm:max-md:text-lg xl:text-xl 2xl:text-2xl min-[1920px]:text-3xl font-bold tracking-wide mb-4 sm:mb-0 ">
 Reglas de Operación y Check List para Desarrolladores
 </h3>
 
 <div class="w-full">
 <article class="flex flex-col sm:flex-row bg-[#DEDEDE] p-5 2xl:p-6 w-full items-center rounded gap-7 sm:gap-0 mb-8 sm:mb-0 sm:justify-between">
- <p class="text-center sm:text-left text-xl sm:text-sm md:text-lg lg:text-xl 2xl:text-3xl sm:w-7/12 lg:w-9/12">
+<p class="text-center sm:text-left text-xl sm:text-sm md:text-lg lg:text-lg xl:text-xl 2xl:text-2xl min-[1920px]:text-3xl sm:w-7/12 lg:w-9/12">
  Reglas de Operación Crédito Integral Cufrisa (pdf)
 
 
  </p>
- <a href="<?php echo get_template_directory_uri(); ?>/assets/pdf/ANEXO A - Reglas de Operacion credito integral.pdf" target="_blank" class="flex sm:flex-row justify-center bg-[#263650] w-11/12 sm:w-4/12 xl:w-3/12 text-center text-white text-xl sm:text-sm md:text-lg lg:text-xl 2xl:text-3xl py-4 2xl:py-6 rounded hover:bg-blue-600 transition-colors duration-300">
+ <a href="<?php echo get_template_directory_uri(); ?>/assets/pdf/ANEXO A - Reglas de Operacion credito integral.pdf" 
+    target="_blank" 
+    class="flex sm:flex-row justify-center bg-[#263650] w-11/12 sm:w-4/12 xl:w-3/12 text-center text-white text-xl sm:text-sm md:text-lg lg:text-xl xl:text-xl 2xl:text-2xl min-[1920px]:text-3xl py-4 2xl:py-6 rounded hover:bg-blue-600 transition-colors duration-300">
   Descargar
   <img src="<?php echo get_template_directory_uri(); ?>/public/descargas.svg" 
        loading="lazy" 
@@ -178,12 +180,12 @@ Reglas de Operación y Check List para Desarrolladores
 </article>
 
 <article class="flex flex-col sm:flex-row bg-white p-5 2xl:p-6 w-full items-center rounded gap-7 sm:gap-0 sm:justify-between">
- <p class="text-center sm:text-left text-xl sm:text-sm md:text-lg lg:text-xl 2xl:text-3xl sm:w-7/12 lg:w-9/12">
+<p class="text-center sm:text-left text-xl sm:text-sm md:text-lg lg:text-lg xl:text-xl 2xl:text-2xl min-[1920px]:text-3xl sm:w-7/12 lg:w-9/12">
   Check List Crédito Integral (pdf)
  </p>
  <a href="<?php echo get_template_directory_uri(); ?>/assets/pdf/Check List Credito Integral 1.0.pdf" 
     target="_blank" 
-    class="flex sm:flex-row justify-center bg-[#263650] w-11/12 sm:w-4/12 xl:w-3/12 text-center text-white text-xl sm:text-sm md:text-lg lg:text-xl 2xl:text-3xl py-4 2xl:py-6 rounded hover:bg-blue-600 transition-colors duration-300">
+    class="flex sm:flex-row justify-center bg-[#263650] w-11/12 sm:w-4/12 xl:w-3/12 text-center text-white text-xl sm:text-sm md:text-lg lg:text-xl xl:text-xl 2xl:text-2xl min-[1920px]:text-3xl py-4 2xl:py-6 rounded hover:bg-blue-600 transition-colors duration-300">
   Descargar
   <img src="<?php echo get_template_directory_uri(); ?>/public/descargas.svg" 
        loading="lazy" 
